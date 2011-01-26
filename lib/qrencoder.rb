009269f069ad65557d78187fdbcbc321dbfa8e3b
Jim Remsik and Joshua Davey <dev+bigtiger+jgdavey@hashrocket.com>
2011-01-26 17:02:37 -0600

    Fix flipped y axis for generated PNGs
    - Save PNGs with a white background
    - Make black points fully opaque

--- lib/qrencoder.rb
+++ lib/qrencoder.rb
@@ -128,13 +128,19 @@ class QRCode
   # Save the QRcode to a PNG file. You can also specify a margin in pixels around
   # the image, although the specification requests it should be at least 4 px.
   def save_png(path, margin=4)
-    canvas = PNG::Canvas.new width + (2*margin), width+(2*margin)
-
+    canvas = PNG::Canvas.new(width + (2*margin), width+(2*margin), PNG::Color::White)
     points.each do |p|
-      canvas.point p[0]+margin, p[1]+margin, PNG::Color::Black
+      x, y = png_coordinates_for_point(p, margin)
+      canvas[x,y] = PNG::Color::Black
     end
-    
     png = PNG.new canvas
     png.save path
+  end
+
+  private
+  def png_coordinates_for_point(point, margin)
+    x = point[0] + margin
+    y = width - 1 - point[1] + margin
+    [x,y]
   end
 end
